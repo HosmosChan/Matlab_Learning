e09ce98d127962c09f120f89130945ee524f65ee
chenhuayang <chenhuayang@gemdale.com>
2019-04-18 14:41:30 +0800

    no message

--- learningClassImage.m
+++ learningClassImage.m
@@ -2,6 +2,6 @@
 learningClassImage.m
 %}
 imgRbg2Gray('01.jpg');% 将彩色图片进行阈值化处理
-waitBar(10e+2);% 延时10e+2 ms
+waitBar(10e+2);% 延时 200 ms
 figure;
 imgGraythresh('./ImageProcessing/Result/result01.png');% 将阈值化处理后的图片进行二值化分割
